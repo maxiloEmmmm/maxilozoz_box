6d7453b4e10a40d72a88b54dece6e026ddce7a47
maxilozoz <2802146520@qq.com>
2020-10-30 22:28:23 +0800

    fix test env

--- test/config_test.dart
+++ test/config_test.dart
@@ -4,7 +4,7 @@ import 'package:maxilozoz_box/modules/config/config.dart';
 void main() {
   test('adds one to input values', () {
     Config config = new Config();
-    //test debug model is prod.
+    //test debug model is not prod.
 
     config.add({
       "a": 1
@@ -14,7 +14,12 @@ void main() {
       "b": 1
     }, inDev: true);
 
-    expect(config.get("a"), 1);
-    expect(config.get("b") == null, true);
+    if (bool.fromEnvironment('dart.vm.product')) {
+      expect(config.get("a"), 1);
+      expect(config.get("b") == null, true);
+    }else {
+      expect(config.get("a") == null, true);
+      expect(config.get("b"), 1);
+    }
   });
 }
